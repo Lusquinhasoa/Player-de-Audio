9cfabf14d4d4149dc47da6191e9953c1faeb4beb
Lucas de Oliveira Alencar <lucasalencaar7@gmail.com>
2022-05-17 14:52:37 -0300

    Mudanças em propriedades
    mudança no style.css
    e local de propriedades do javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var music = document.querySelector('audio');
-var indexMusica = 0;
+var musicas = document.querySelector('audio');
+var indexMusica = 2;
 
 var duracao = document.querySelector('.fim');
 var imagem = document.querySelector('img');
@@ -8,28 +8,28 @@ var artista = document.querySelector('.descrição i');
 
 render(indexMusica);
 
-var musics = [
+var musicas = [
     {título: 'Eletrônica', artista: 'Mini Vandals',src:'músicas/Gunpowder Tea - Mini Vandals.mp3', img:'imagens/bogomil-mihaylov-ekHSHvgr27k-unsplash.jpg'},
-    {título: 'Eletrônica', artista: 'Yung Logos',src:'músicas/Houser - Yung Logos.mp3', img:'imagens/bass.jpg'},
-    {título: 'Eletrônica', artista: 'NEFFEX',src:'músicas/Something You Could Never Own (Clean) - NEFFEX.mp3', img:'imagens/partit.jpg'}
+    {título: 'Eletrônica', artista: 'NEFFEX',src:'músicas/Something You Could Never Own (Clean) - NEFFEX.mp3'},
+    {título: 'Eletrônica', artista: 'Yung Logos',src:'músicas/Houser - Yung Logos.mp3', img:'imagens/bass.jpg'}
 ];
 
 document.querySelector('.botao-play').addEventListener('click', tocar);
 
 document.querySelector('.botao-pause').addEventListener('click', pausar);
 
-music.addEventListener('timeupdate', attbarrinha);
+musicas.addEventListener('timeupdate', attbarrinha);
 
 document.querySelector('.anterior').addEventListener('click', () => {
-    indexMusica--;
+    indexMusica --;
     if(indexMusica < 0){
         indexMusica = 2;
     }
     render(indexMusica);
 });
 
-document.querySelector('.próximo').addEventListener('click', () => {
-    indexMusica++;
+document.querySelector('.proximo').addEventListener('click', () => {
+    indexMusica--;
     if(indexMusica > 2){
         indexMusica = 0;
     }
@@ -37,32 +37,32 @@ document.querySelector('.próximo').addEventListener('click', () => {
 });
 
 function render(index){
-    music.setAttribute('src', músicas[index].src);
-    music.addEventListener('loadeddata', () => {
-        nomeMus.textContent = músicas[index].titulo;
-        artista.textContent = músicas[index].artista;
-        imagem.src = músicas[index].img;
-        duracao.textContent = sgsTomin(Math.floor(music.duration));
+    musicas.setAttribute('src', musicas[index].src);
+    musicas.addEventListener('loadeddata', () => {
+        nomeMus.textContent = musicas[index].titulo;
+        artista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracao.textContent = sgsTomin(Math.floor(musicas.duration));
     })
 }
 
 function tocar(){
-    music.play();
+    musicas.play();
     document.querySelector('.botao-pause').style.display = 'block';
     document.querySelector('.botao-play').style.display = 'none';
 }
 
 function pausar(){
-    music.pause();
-    document.querySelector('.botao-pause').style.display = 'none';
-    document.querySelector('.botao-play').style.display = 'block';
+    musicas.pause();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
 }
 
 function attbarrinha(){
     var barrinha = document.querySelector('progress');
-    barrinha.style.width = Math.floor((music.currentTime / music.duration) * 100) + '%';
+    barrinha.style.width = Math.floor((musicas.currentTime / musicas.duration) * 100) + '%';
     var tempo = document.querySelector('.inicio');
-    tempo.textContent = Math.floor(music.currentTime);
+    tempo.textContent = Math.floor(musicas.currentTime);
 }
 function sgsTomin(sgs){
     var min = Math.floor(sgs/60);
